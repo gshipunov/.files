1798d6f536cd8ec8557aabb6c65fc57dcea4546e
Grigory Shipunov <blame@oxapentane.com>
2021-12-02 13:44:46 +0100

    highlight host if connected over ssh

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -1,1 +1,5 @@
-export PS1="\[\e[1;36m\]\u@\h\[\e[1;0m\]:\[\e[1;92m\]\w\[\e[1;0m\]$ "
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+	export PS1="\[\e[1;36m\]\u@\[\e[1;35m\]\h\[\e[1;0m\]:\[\e[1;92m\]\w\[\e[1;0m\]$ "
+else
+	export PS1="\[\e[1;36m\]\u@\h\[\e[1;0m\]:\[\e[1;92m\]\w\[\e[1;0m\]$ "
+fi
