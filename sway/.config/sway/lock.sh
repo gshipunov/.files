0b5fac6216baf7f55641aa5a8cb946dfc8d132c5
Grigory Shipunov <blame@oxapentane.com>
2023-01-08 02:32:53 +0100

    new colors

--- sway/.config/sway/lock.sh
+++ sway/.config/sway/lock.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 img="$HOME/.cache/swaylock.png"
+#img="$HOME/.files/sway/.config/sway/lock-fullhd.png"
 
 grim $img
-gm convert $img -scale 5% -scale 2000% $img
+#gm convert $img -scale 5% -scale 2000% $img
+gm convert $img -blur 0x10 $img
 swaylock -eFfk -i $img -s fill
 
 rm -f $img
