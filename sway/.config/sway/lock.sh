cae6b5663d70fc84af31334011d0896c86b5a6ab
Grigory Shipunov <blame@oxapentane.com>
2023-03-16 14:40:47 +0100

    breaking changes in i3status-rust

--- sway/.config/sway/lock.sh
+++ sway/.config/sway/lock.sh
@@ -4,8 +4,8 @@ img="$HOME/.cache/swaylock.png"
 #img="$HOME/.files/sway/.config/sway/lock-fullhd.png"
 
 grim $img
-#gm convert $img -scale 5% -scale 2000% $img
-gm convert $img -blur 0x10 $img
-swaylock -eFfk -i $img -s fill
+gm convert $img -scale 5% -scale 2000% $img
+#gm convert $img -blur 0x10 $img
+swaylock -Ffk -i $img -s fill
 
 rm -f $img
