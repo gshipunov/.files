1fe8b52a206a4c7ad44e0a11af550886789b94d7
Grigory Shipunov <blame@oxapentane.com>
2023-07-24 00:03:33 +0200

    don't sign commits by default

--- bash/.bashrc.d/alias.sh
+++ bash/.bashrc.d/alias.sh
@@ -1,6 +1,29 @@
+# ls
+alias l="ls -l"
+alias la="ls -la"
+alias ll="ls -lah"
+alias lt="ls --tree"
+
+# tmux
+if [ -x "$(command -v tmux)" ]; then
+    function tn {
+        if [ $# -eq 0 ]; then
+            tmux new-session -s $(basename $(pwd))
+        else
+            tmux new-session -s $1
+        fi
+    }
+    alias tl="tmux list-sessions"
+    alias ta="tmux attach -t"
+fi
+
 # LaTeX
-alias luamk="latexmk -pdf -interaction=nonstopmode -pdflatex=lualatex"
-alias lmk="latexmk -pdf -interaction=nonstopmode"
+if [ -x "$(command -v latexmk)" ]; then
+    if [ -x "$(command -v lualatex)" ]; then
+        alias luamk="latexmk -pdf -interaction=nonstopmode -pdflatex=lualatex"
+    fi
+    alias lmk="latexmk -pdf -interaction=nonstopmode"
+fi
 
 # editor
 if [ -x "$(command -v nvim)" ]; then
--- bash/.bashrc.d/vars.sh
+++ bash/.bashrc.d/vars.sh
@@ -1,1 +1,4 @@
 export JULIA_NUM_THREADS=auto
+export EDITOR=nvim
+# export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+export GPG_TTY=$(tty)
